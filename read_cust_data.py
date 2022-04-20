33ff016aa25afd4503ded6beb287c755e81bd972
smargarin <smargarin@gmail.com>
2022-04-20 19:40:41 -0400

    "Add Config paersr"

---
+++ read_cust_data.py
@@ -0,0 +1,7 @@
+import ConfigurationParser
+cp = ConfigurationParser()
+expected_names = ['CUSTOMER_A', 'CUSTOMER_B']
+parsed_names = cp.parseCustomerNames()
+#self.assertEqual(list, type(parsed_names)
+#self.assertEqual(expected_names, parsed_names)
+
